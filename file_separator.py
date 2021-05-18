e67da8523ea48f1ca34222a8deaa33d2d62a79da
Kornelia Noculak <noculakkx@gmail.com>
2021-05-18 20:20:12 +0200

    Implemented method get_all_files

--- file_separator.py
+++ file_separator.py
@@ -1,6 +1,9 @@
 from dataclasses import dataclass
-from typing import List
+from typing import List, Tuple
 from enum import Enum
+from datetime import datetime
+import os
+import time
 
 
 class DateDirFormat(Enum):
@@ -24,8 +27,28 @@ class FileSeparator:
 
     @classmethod
     def run(cls, fso: FileSeparatorOptions):
-        pass
+        if not os.path.isdir(fso.base_dir) or not os.path.isdir(fso.target_dir):
+            raise ValueError('komunikat folder nie istnieje')
 
     @classmethod
     def preview(cls, fso: FileSeparatorOptions):
         pass
+
+    @classmethod
+    def get_all_files(cls, root_dir: str) -> List[Tuple[str, datetime, int]]:
+        """
+        :return: list of files with paths relative to root_dir, creation dates, sizes
+        """
+        files_list = []
+
+        for dir_path, _, files in os.walk(root_dir):
+            for f in files:
+                f_path = os.path.join(dir_path, f)
+                f_size = os.path.getsize(f_path)
+
+                f_time = time.ctime(os.path.getctime(f_path))
+                f_date = datetime.strptime(f_time, '%a %b %d %H:%M:%S %Y')
+
+                files_list.append((f_path, f_date, f_size))
+
+        return files_list
